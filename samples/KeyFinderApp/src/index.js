9da4a01dfe62d3470bc34e1146fad3bb5ad31124
Andy Ng <andyng30@gmail.com>
2016-04-10 16:30:23 -0700

    Updated changes to the onIntent method to setKeyLocationIntent to match with the IntentSchema.json. Also, did some clean up to the code.

--- samples/KeyFinderApp/src/index.js
+++ samples/KeyFinderApp/src/index.js
@@ -1,30 +1,37 @@
 /**
- * This is a skill that reminds your key location. 
- * Built with the Amazon Alexa Skills Kit.
+ * This is an Alexa skill allow user to set and remind them the location of their key 
+ * Built using the Amazon Alexa Skills Kit.
  * 
  */
 exports.handler = function (event, context) {
     try {
         console.log("event.session.application.applicationId=" + event.session.application.applicationId);
-
+        
+        if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.a7cd8c09-3ea3-4d5a-ae6c-1e9fa99c056b") {
+             context.fail("Invalid Application ID");
+        }
+        
         if (event.session.new) {
             onSessionStarted({requestId: event.request.requestId}, event.session);
         }
-
+        
         if (event.request.type === "LaunchRequest") {
             onLaunch(event.request, event.session,function callback(sessionAttributes, speechletResponse) {
                     context.succeed(buildResponse(sessionAttributes, speechletResponse));
                 });
+                
         } else if (event.request.type === "IntentRequest") {
             onIntent(event.request,
                 event.session,
                 function callback(sessionAttributes, speechletResponse) {
                     context.succeed(buildResponse(sessionAttributes, speechletResponse));
                 });
+                
         } else if (event.request.type === "SessionEndedRequest") {
             onSessionEnded(event.request, event.session);
             context.succeed();
         }
+        
     } catch (e) {
         context.fail("Exception: " + e);
     }
@@ -60,7 +67,7 @@ function onIntent(intentRequest, session, callback) {
         intentName = intentRequest.intent.name;
 
     // Dispatch to your skill's intent handlers
-    if ("MyKeyLocationIntent" === intentName) {
+    if ("SetKeyLocationIntent" === intentName) {
     	setKeyInSession(intent, session, callback);
     } else if ("WhereMyKeyIntent" === intentName) {
         getKeyFromSession(intent, session, callback);
@@ -89,28 +96,28 @@ function getWelcomeResponse(session, callback) {
     // If we wanted to initialize the session to have some attributes we could add those here.
     var sessionAttributes = {}
     var cardTitle = "Welcome to Key Finder";
-    //var speechOutput = "Welcome to the Key Finder Application. " +
-    //    "Please let me help you find your key, tell me where is your key?";
-        var speechOutput = "";
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
+    var speechOutput = "";
     var repromptText = "";
     var shouldEndSession = false;
-
-    if (!session.attributes) 
+    
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    if (!session.new) 
     {
-        speechOutput = "Welcome to the Key Finder Application. " +
-        "Please let me help you find your key, tell me where is your key?";
-        repromptText = "Please tell me where is your key?";
-    }
-    else {
         keyLocation = session.attributes.keyLocation;
         sessionAttributes = createKeyLocationAttributes(keyLocation);
         speechOutput = "Your key is located in " + keyLocation;
     }
+    else 
+    {
+        speechOutput = "Welcome to the Key Finder Application. " +
+        "Please let me help you find your key, tell me where is your key?";
+    
+        repromptText = "I did not understand you, Please tell me where is your key?";
+    }
 
     callback(sessionAttributes,
-        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, false));
 }
 
 /**
@@ -121,24 +128,25 @@ function setKeyInSession(intent, session, callback) {
     var keyLocationSlot = intent.slots.Location;
     var repromptText = "";
     var sessionAttributes = {};
-    var shouldEndSession = false;
     var speechOutput = "";
+    var shouldEndSession;
+    var keyLocation;
 
-    if (keyLocationSlot) {
-        var keyLocation = keyLocationSlot.value;
-        //sessionAttributes = session.setAttribute(keyLocation);
+    if(keyLocationSlot) 
+    {
+        keyLocation = keyLocationSlot.value;
         sessionAttributes = createKeyLocationAttributes(keyLocation);
-        speechOutput = "Now, I know where your key is located, in " + keyLocation + ". You can ask me " +
-            "where is my key?";
-        repromptText = "You can ask me where your key is located by saying, where is my key?";
-    } else {
+        speechOutput = "Your key is found in the " + keyLocation + ". You can ask key finder, where is my key?";
+        repromptText = "I did not understand you, you can ask key finder, where is my key? or say stop to exit.";
+    }
+    else {
         speechOutput = "I'm not sure where your key is located. Please try again";
         repromptText = "I'm not sure where your key is located. You can tell me where " +
             "is your key located, my key is located in kitchen";
     }
 
     callback(sessionAttributes,
-         buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+         buildSpeechletResponse(cardTitle, speechOutput, repromptText, false));
          
 }
 
@@ -155,18 +163,22 @@ function getKeyFromSession(intent, session, callback) {
     var sessionAttributes = {};
     var shouldEndSession = false;
     var speechOutput = "";
-
-    if (session.attributes) {
+    
+    if (session.attributes) 
+    {
         keyLocation = session.attributes.keyLocation;
         sessionAttributes = createKeyLocationAttributes(keyLocation);
-
     }
 
     if (keyLocation) {
-        speechOutput = "Your key is found in " + keyLocation + ". Say stop to exit";
-        //shouldEndSession = true;
+        speechOutput = "Your key is found in " + keyLocation + ". You can say stop to exit";
+        repromptText = "I did not understand you. You can ask key finder, where is my key? or say stop to exit.";
+        shouldEndSession = false;
+        
     } else {
-        speechOutput = "I'm not sure where your key is located, you can say, my key is in kitchen";
+        speechOutput = "I'm not sure where your key is located. You can say, key finder my key is located in the kitchen ";
+        repromptText = "You can say, my key is in kitchen";
+        shouldEndSession = false;
     }
 
     // Setting repromptText to null signifies that we do not want to reprompt the user.
@@ -180,14 +192,17 @@ function helpIntent(intent, session, callback) {
 		var keyLocation;
 		var sessionAttributes = {};
 	    var cardTitle = "KeyFinder - Help";
-	    var speechOutput = "With Key Finder App, you can remember where you last place your key. " +
-	        "For example, you could say, where is my key, or you can say stop to exit.";
-	    var repromptText = "Please let me help you find your key by saying, " +
-	        "where is my key?";
+	    var speechOutput = "Key Finder skill will remember where you last place your key. " +
+	        "To use the Key Finder, try saying, where is my key? , or you can say stop to exit.";
+	    var repromptText = "Please let me know how I may help you find your key?";
+	        //, " +
+	        //"where is my key?";
 	    var shouldEndSession = false;
 
 	    if (session.attributes) {
 	        keyLocation = session.attributes.keyLocation;
+	       sessionAttributes = createKeyLocationAttributes(keyLocation);
+
 	    }
 	    
 	    callback(sessionAttributes,
@@ -223,8 +238,7 @@ function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
                 text: repromptText
             }
         },
-        shouldEndSession: false,
-        session: this.session
+        shouldEndSession: shouldEndSession,
     };
 }
 
